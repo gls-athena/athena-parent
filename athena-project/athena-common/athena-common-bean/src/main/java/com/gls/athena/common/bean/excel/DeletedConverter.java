bacce44585ac7011ebcf2771c44209b004965afa
george1215 <george510257@gmail.com>
2025-01-03 17:25:19 +0800

    优化代码

--- athena-project/athena-common/athena-common-bean/src/main/java/com/gls/athena/common/bean/excel/DeletedConverter.java
+++ athena-project/athena-common/athena-common-bean/src/main/java/com/gls/athena/common/bean/excel/DeletedConverter.java
@@ -8,16 +8,29 @@ import com.alibaba.excel.metadata.data.WriteCellData;
 import com.alibaba.excel.metadata.property.ExcelContentProperty;
 
 /**
- * 删除标记转换器
+ * Excel 删除标记转换器
+ * 用于在 Excel 导入导出时转换删除状态
+ * - Excel中的"已删除"对应Java中的true
+ * - Excel中的"正常"对应Java中的false
  *
  * @author george
  */
 public class DeletedConverter implements Converter<Boolean> {
 
     /**
-     * 支持的Java类型
+     * Excel中表示删除状态的文本
+     */
+    private static final String DELETED_TEXT = "已删除";
+
+    /**
+     * Excel中表示正常状态的文本
+     */
+    private static final String NORMAL_TEXT = "正常";
+
+    /**
+     * 获取Java数据类型
      *
-     * @return Java类型
+     * @return 返回Boolean.class，表示转换器支持布尔类型
      */
     @Override
     public Class<?> supportJavaTypeKey() {
@@ -25,9 +38,9 @@ public class DeletedConverter implements Converter<Boolean> {
     }
 
     /**
-     * 支持的Excel类型
+     * 获取Excel单元格数据类型
      *
-     * @return Excel类型
+     * @return 返回STRING类型，表示Excel中以字符串形式存储
      */
     @Override
     public CellDataTypeEnum supportExcelTypeKey() {
@@ -35,29 +48,32 @@ public class DeletedConverter implements Converter<Boolean> {
     }
 
     /**
-     * 转换为Java数据
+     * 将Excel数据转换为Java数据
      *
      * @param cellData            Excel单元格数据
-     * @param contentProperty     内容属性
-     * @param globalConfiguration 全局配置
-     * @return Java数据
+     * @param contentProperty     单元格配置属性
+     * @param globalConfiguration 全局配置信息
+     * @return true表示已删除，false表示未删除
      */
     @Override
     public Boolean convertToJavaData(ReadCellData<?> cellData, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) {
-        return "已删除".equals(cellData.getStringValue());
+        if (cellData == null || cellData.getStringValue() == null) {
+            return Boolean.FALSE;
+        }
+        return DELETED_TEXT.equals(cellData.getStringValue().trim());
     }
 
     /**
-     * 转换为Excel数据
+     * 将Java数据转换为Excel数据
      *
-     * @param value               Java数据
-     * @param contentProperty     内容属性
-     * @param globalConfiguration 全局配置
-     * @return Excel数据
+     * @param value               布尔值，true表示已删除，false表示正常
+     * @param contentProperty     单元格配置属性
+     * @param globalConfiguration 全局配置信息
+     * @return 转换后的Excel单元格数据：已删除/正常
      */
     @Override
     public WriteCellData<?> convertToExcelData(Boolean value, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) {
-        return value ? new WriteCellData<>("已删除") : new WriteCellData<>("正常");
+        String text = Boolean.TRUE.equals(value) ? DELETED_TEXT : NORMAL_TEXT;
+        return new WriteCellData<>(text);
     }
-
 }
