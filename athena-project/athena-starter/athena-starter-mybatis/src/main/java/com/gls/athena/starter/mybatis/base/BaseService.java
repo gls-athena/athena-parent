fdc2ffa988f3672789e367aba92d8327f8086a3a
george1215 <george510257@gmail.com>
2025-03-18 18:12:38 +0800

    添加注释

--- athena-project/athena-starter/athena-starter-mybatis/src/main/java/com/gls/athena/starter/mybatis/base/BaseService.java
+++ athena-project/athena-starter/athena-starter-mybatis/src/main/java/com/gls/athena/starter/mybatis/base/BaseService.java
@@ -31,29 +31,59 @@ public abstract class BaseService<V extends BaseVo, E extends BaseEntity,
     protected C converter;
 
     /**
-     * 插入
+     * 插入一个VO对象并将其转换为实体对象进行保存，最后将保存后的实体对象转换回VO对象返回。
      *
-     * @param vo VO 对象
-     * @return VO 对象
+     * @param vo 要插入的VO对象，不能为null
+     * @return 保存后的VO对象
+     * @throws IllegalArgumentException 如果传入的VO对象为null，抛出此异常
+     * @throws RuntimeException         如果插入操作过程中发生异常，抛出此异常
      */
     @Override
     public V insert(V vo) {
-        E entity = converter.convert(vo);
-        save(entity);
-        return converter.reverse(entity);
+        // 检查传入的VO对象是否为null，若为null则抛出异常
+        if (vo == null) {
+            throw new IllegalArgumentException("VO对象不能为null");
+        }
+
+        try {
+            // 将VO对象转换为实体对象
+            E entity = converter.convert(vo);
+            // 保存实体对象
+            save(entity);
+            // 将保存后的实体对象转换回VO对象并返回
+            return converter.reverse(entity);
+        } catch (Exception e) {
+            // 记录日志或抛出自定义异常
+            throw new RuntimeException("插入操作失败", e);
+        }
     }
 
     /**
-     * 更新
+     * 更新 VO 对象对应的实体，并返回更新后的 VO 对象。
      *
-     * @param vo VO 对象
-     * @return VO 对象
+     * @param vo 需要更新的 VO 对象，不能为 null
+     * @return 更新后的 VO 对象
+     * @throws IllegalArgumentException 如果 VO 对象或转换器为 null，抛出此异常
+     * @throws RuntimeException         如果更新操作过程中发生异常，抛出此异常
      */
     @Override
     public V update(V vo) {
-        E entity = converter.convert(vo);
-        updateById(entity);
-        return converter.reverse(entity);
+        // 非空检查：确保 VO 对象和转换器都不为 null
+        if (vo == null || converter == null) {
+            throw new IllegalArgumentException("VO对象或转换器不能为空");
+        }
+
+        try {
+            // 将 VO 对象转换为实体对象
+            E entity = converter.convert(vo);
+            // 更新实体对象
+            updateById(entity);
+            // 将更新后的实体对象转换回 VO 对象并返回
+            return converter.reverse(entity);
+        } catch (Exception e) {
+            // 异常处理：捕获并包装异常，抛出运行时异常
+            throw new RuntimeException("更新操作失败", e);
+        }
     }
 
     /**
