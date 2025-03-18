edb1b9220f8e9b13d485d0e9852e3a84ab95f0e4
george1215 <george510257@gmail.com>
2025-03-18 16:41:36 +0800

    添加注释

--- athena-project/athena-starter/athena-starter-dynamic-datasource/src/main/java/com/gls/athena/starter/dynamic/datasource/support/DefaultJdbcDataSourceProvider.java
+++ athena-project/athena-starter/athena-starter-dynamic-datasource/src/main/java/com/gls/athena/starter/dynamic/datasource/support/DefaultJdbcDataSourceProvider.java
@@ -44,20 +44,24 @@ public class DefaultJdbcDataSourceProvider extends AbstractJdbcDataSourceProvide
 
     /**
      * 执行SQL查询并获取数据源配置信息
+     * <p>
+     * 该方法通过执行预定义的SQL查询语句，从数据库中获取数据源配置信息，并将其转换为数据源配置映射表。
+     * 同时，该方法还会添加默认的主数据源配置到映射表中。
      *
-     * @param statement 数据库查询语句对象
-     * @return 数据源配置映射表
+     * @param statement 数据库查询语句对象，用于执行SQL查询
+     * @return 包含所有数据源配置信息的映射表，键为数据源名称，值为数据源配置属性
      * @throws SQLException 当数据库操作发生异常时抛出
      */
     @Override
     protected Map<String, DataSourceProperty> executeStmt(Statement statement) throws SQLException {
         Map<String, DataSourceProperty> dataSourcePropertiesMap;
 
+        // 执行SQL查询并将结果集转换为数据源配置映射表
         try (ResultSet rs = statement.executeQuery(dynamicDataSourceProperties.getQueryDsSql())) {
             dataSourcePropertiesMap = toDataSourcePropertiesMap(rs);
         }
 
-        // 添加默认主数据源配置
+        // 添加默认主数据源配置到映射表中
         dataSourcePropertiesMap.put(IConstants.DEFAULT_DATASOURCE_NAME, createDefaultDataSourceProperty());
 
         return dataSourcePropertiesMap;
@@ -65,15 +69,21 @@ public class DefaultJdbcDataSourceProvider extends AbstractJdbcDataSourceProvide
 
     /**
      * 将查询结果转换为数据源配置对象
+     * <p>
+     * 该方法从数据库查询结果集中提取数据源配置信息，并将其转换为数据源配置对象的映射表。
+     * 每个数据源配置对象包含驱动类名、URL、用户名、密码等信息，并且设置为懒加载模式。
      *
-     * @param rs 数据库查询结果集
-     * @return 数据源配置映射表
+     * @param rs 数据库查询结果集，包含数据源配置信息
+     * @return 数据源配置映射表，键为数据源名称，值为对应的数据源配置对象
      * @throws SQLException 当结果集解析发生异常时抛出
      */
     private Map<String, DataSourceProperty> toDataSourcePropertiesMap(ResultSet rs) throws SQLException {
+        // 初始化数据源配置映射表
         Map<String, DataSourceProperty> dataSourcePropertiesMap = new HashMap<>(16);
 
+        // 遍历结果集，提取数据源配置信息
         while (rs.next()) {
+            // 创建数据源配置对象并设置属性
             DataSourceProperty property = new DataSourceProperty();
             property.setDriverClassName(rs.getString(dynamicDataSourceProperties.getDsDriverColumn()));
             property.setUrl(rs.getString(dynamicDataSourceProperties.getDsUrlColumn()));
@@ -81,25 +91,37 @@ public class DefaultJdbcDataSourceProvider extends AbstractJdbcDataSourceProvide
             property.setPassword(rs.getString(dynamicDataSourceProperties.getDsPasswordColumn()));
             property.setLazy(true);
 
+            // 获取数据源名称并将其作为键，数据源配置对象作为值存入映射表
             String dsName = rs.getString(dynamicDataSourceProperties.getDsNameColumn());
             dataSourcePropertiesMap.put(dsName, property);
         }
 
+        // 返回数据源配置映射表
         return dataSourcePropertiesMap;
     }
 
     /**
      * 创建默认数据源配置
+     * <p>
+     * 该方法通过从全局配置中获取数据源的相关属性（如驱动类名、URL、用户名、密码等），
+     * 并将其设置到一个新的 {@link DataSourceProperty} 对象中。同时，将数据源的懒加载属性设置为 true。
      *
-     * @return 默认数据源配置对象
+     * @return 返回一个配置了默认属性的 {@link DataSourceProperty} 对象
      */
     private DataSourceProperty createDefaultDataSourceProperty() {
+        // 创建新的数据源配置对象
         DataSourceProperty property = new DataSourceProperty();
+
+        // 从全局配置中获取并设置数据源的相关属性
         property.setDriverClassName(dataSourceProperties.getDriverClassName());
         property.setUrl(dataSourceProperties.getUrl());
         property.setUsername(dataSourceProperties.getUsername());
         property.setPassword(dataSourceProperties.getPassword());
+
+        // 设置数据源的懒加载属性为 true
         property.setLazy(true);
+
         return property;
     }
+
 }
--- athena-project/athena-starter/athena-starter-dynamic-datasource/src/main/java/com/gls/athena/starter/dynamic/datasource/support/DynamicDataSourceConfig.java
+++ athena-project/athena-starter/athena-starter-dynamic-datasource/src/main/java/com/gls/athena/starter/dynamic/datasource/support/DynamicDataSourceConfig.java
@@ -15,12 +15,14 @@ import org.springframework.context.annotation.Bean;
 public class DynamicDataSourceConfig {
 
     /**
-     * 默认数据源提供者
+     * 创建并返回默认的JDBC数据源提供者。
+     * 该函数根据配置动态数据源是否启用，决定是否创建默认的JDBC数据源提供者。
+     * 如果配置中`athena.dynamic.datasource.enabled`为`true`或未配置，则创建该提供者。
      *
-     * @param defaultDataSourceCreator    默认数据源创建器
-     * @param dataSourceProperties        数据源配置
-     * @param dynamicDataSourceProperties 动态数据源配置
-     * @return 数据源提供者
+     * @param defaultDataSourceCreator    默认数据源创建器，用于创建数据源实例。
+     * @param dataSourceProperties        数据源配置，包含数据源的基本配置信息。
+     * @param dynamicDataSourceProperties 动态数据源配置，包含动态数据源的特定配置信息。
+     * @return 返回一个配置好的默认JDBC数据源提供者实例。
      */
     @Bean
     @ConditionalOnProperty(prefix = "athena.dynamic.datasource", name = "enabled", havingValue = "true", matchIfMissing = true)
@@ -29,4 +31,5 @@ public class DynamicDataSourceConfig {
                                                                        DynamicDataSourceProperties dynamicDataSourceProperties) {
         return new DefaultJdbcDataSourceProvider(defaultDataSourceCreator, dataSourceProperties, dynamicDataSourceProperties);
     }
+
 }
