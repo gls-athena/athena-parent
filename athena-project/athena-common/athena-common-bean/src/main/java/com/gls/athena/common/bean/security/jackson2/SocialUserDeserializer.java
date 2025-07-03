c1d52d9b46a33014942c46ee2cebc57116b28277
george1215 <george510257@gmail.com>
2025-07-03 09:27:46 +0800

    优化spring security client

--- athena-project/athena-common/athena-common-bean/src/main/java/com/gls/athena/common/bean/security/jackson2/SocialUserDeserializer.java
+++ athena-project/athena-common/athena-common-bean/src/main/java/com/gls/athena/common/bean/security/jackson2/SocialUserDeserializer.java
@@ -14,13 +14,27 @@ import java.util.Optional;
  * @author lizy19
  */
 public class SocialUserDeserializer extends BaseDeserializer<SocialUser> {
+    /**
+     * 根据JSON数据创建SocialUser实例
+     * 此方法用于解析表示社交用户信息的JSON数据，并将解析后的数据填充到SocialUser对象中
+     * 它处理的JSON数据可能包含用户属性、用户名、注册ID、关联的用户对象以及绑定状态
+     *
+     * @param mapper ObjectMapper实例，用于将JSON数据映射到Java对象
+     * @param node   包含社交用户信息的JsonNode节点
+     * @return 返回一个填充了从JSON数据中提取的信息的SocialUser实例
+     */
     @Override
     protected SocialUser createInstance(ObjectMapper mapper, JsonNode node) {
         SocialUser socialUser = new SocialUser();
+        // 尝试从JSON数据中获取并设置socialUser的attributes属性
         Optional.ofNullable(node.get("attributes")).map(attributes -> mapper.convertValue(attributes, Map.class)).ifPresent(socialUser::setAttributes);
+        // 尝试从JSON数据中获取并设置socialUser的name属性
         Optional.ofNullable(node.get("name")).map(JsonNode::asText).ifPresent(socialUser::setName);
+        // 尝试从JSON数据中获取并设置socialUser的registrationId属性
         Optional.ofNullable(node.get("registrationId")).map(JsonNode::asText).ifPresent(socialUser::setRegistrationId);
+        // 尝试从JSON数据中获取并设置socialUser的user属性
         Optional.ofNullable(node.get("user")).map(user -> mapper.convertValue(user, User.class)).ifPresent(socialUser::setUser);
+        // 尝试从JSON数据中获取并设置socialUser的bindStatus属性
         Optional.ofNullable(node.get("bindStatus")).map(JsonNode::asBoolean).ifPresent(socialUser::setBindStatus);
         return socialUser;
     }
