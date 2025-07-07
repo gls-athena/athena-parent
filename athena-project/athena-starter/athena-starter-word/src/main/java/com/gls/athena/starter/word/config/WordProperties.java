0479b8aef7311560dc69ef348d81dbfe102547fa
george1215 <george510257@gmail.com>
2025-07-07 16:21:00 +0800

    优化word starter

--- athena-project/athena-starter/athena-starter-word/src/main/java/com/gls/athena/starter/word/config/WordProperties.java
+++ athena-project/athena-starter/athena-starter-word/src/main/java/com/gls/athena/starter/word/config/WordProperties.java
@@ -12,23 +12,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "athena.word")
 public class WordProperties {
 
-    /**
-     * 默认文档标题
-     */
-    private String defaultTitle = "";
-
-    /**
-     * 默认模板路径
-     */
-    private String defaultTemplate = "";
-
-    /**
-     * 默认是否分页
-     */
-    private boolean pagination = true;
-
-    /**
-     * 模板基础路径
-     */
-    private String templateBasePath = "templates/word/";
 }
