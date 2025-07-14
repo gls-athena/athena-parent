ab56d03eb6b418c76a757680ca27eef58f5efa23
george <george510257@gmail.com>
2025-07-14 18:20:30 +0800

    优化基础值对象注释

--- athena-project/athena-starter/athena-starter-core/src/main/java/com/gls/athena/starter/core/async/AsyncTaskType.java
+++ athena-project/athena-starter/athena-starter-core/src/main/java/com/gls/athena/starter/core/async/AsyncTaskType.java
@@ -5,40 +5,54 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
- * 异步任务类型
+ * 异步任务类型枚举
+ * <p>
+ * 定义系统中支持的异步任务类型，包括数据导入、导出等操作类型。
+ * 每个枚举值都包含对应的编码和名称，用于在系统中标识不同类型的异步任务。
+ * </p>
  *
  * @author george
+ * @since 1.0.0
  */
 @Getter
 @RequiredArgsConstructor
 public enum AsyncTaskType implements IEnum<Integer> {
     /**
-     * 导入
+     * 数据导入任务
+     * <p>用于标识批量数据导入相关的异步任务</p>
      */
     IMPORT(0, "导入"),
+
     /**
-     * 导出
+     * 数据导出任务
+     * <p>用于标识批量数据导出相关的异步任务</p>
      */
     EXPORT(1, "导出"),
+
     /**
-     * 其它
+     * 其他类型任务
+     * <p>用于标识除导入导出外的其他异步任务类型</p>
      */
     OTHER(2, "其它");
 
     /**
-     * 异步任务类型
+     * 异步任务类型编码
+     * <p>每个任务类型的唯一标识码</p>
      */
     private final Integer code;
+
     /**
      * 异步任务类型名称
+     * <p>任务类型的中文描述名称</p>
      */
     private final String name;
 
     /**
-     * 根据给定的代码值获取对应的异步任务类型枚举实例。
+     * 根据编码获取异步任务类型枚举实例
      *
-     * @param code 异步任务类型的代码值，用于查找对应的枚举实例。
-     * @return 返回与给定代码值匹配的异步任务类型枚举实例。如果未找到匹配的枚举实例，则返回null。
+     * @param code 异步任务类型编码，不能为null
+     * @return 对应的异步任务类型枚举实例，如果编码不存在则返回null
+     * @see IEnum#of(Class, Object)
      */
     public static AsyncTaskType getByCode(Integer code) {
         return IEnum.of(AsyncTaskType.class, code);
