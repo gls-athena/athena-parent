ff8fef837dd1a2cc6c2bfa49cb6ccd1678e2c6e6
george <george510257@gmail.com>
2025-07-23 19:49:38 +0800

    优化插件

--- athena-project/athena-security/athena-security-captcha/src/main/java/com/gls/athena/security/captcha/filter/CaptchaFilter.java
+++ athena-project/athena-security/athena-security-captcha/src/main/java/com/gls/athena/security/captcha/filter/CaptchaFilter.java
@@ -10,10 +10,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.boot.web.servlet.filter.OrderedFilter;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -29,6 +29,7 @@ import java.io.IOException;
  *
  * @author george
  */
+@Component
 @RequiredArgsConstructor
 public class CaptchaFilter extends OncePerRequestFilter implements OrderedFilter {
 
@@ -37,8 +38,7 @@ public class CaptchaFilter extends OncePerRequestFilter implements OrderedFilter
      */
     private final CaptchaProviderManager captchaProviderManager;
 
-    @Setter
-    private AuthenticationFailureHandler authenticationFailureHandler = this::onAuthenticationFailure;
+    private final AuthenticationFailureHandler authenticationFailureHandler = this::onAuthenticationFailure;
 
     /**
      * 执行验证码过滤逻辑
