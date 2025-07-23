aa89f81750e9e9ce160bf39461b77292b48a5bc7
george <george510257@gmail.com>
2025-07-23 15:22:14 +0800

    优化插件

--- athena-project/athena-security/athena-security-captcha/src/main/java/com/gls/athena/security/captcha/CaptchaAutoConfig.java
+++ athena-project/athena-security/athena-security-captcha/src/main/java/com/gls/athena/security/captcha/CaptchaAutoConfig.java
@@ -1,6 +1,7 @@
 package com.gls.athena.security.captcha;
 
 import com.gls.athena.security.captcha.config.CaptchaProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ComponentScan
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @EnableConfigurationProperties(CaptchaProperties.class)
 public class CaptchaAutoConfig {
 }
