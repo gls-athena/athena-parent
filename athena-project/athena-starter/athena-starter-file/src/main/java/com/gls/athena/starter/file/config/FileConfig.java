bdf9e9766e9691a25f9c5c1ba53c007e6f23667d
george <george510257@gmail.com>
2025-10-01 22:40:59 +0800

    feat(file): 添加文件存储管理器配置支持
    - 新增基于内存的文件信息管理器 Bean 配置
    - 添加本地文件存储管理器的条件化配置
    - 实现 OSS 文件存储管理器的自动装配逻辑- 引入对阿里云 OSS 存储的支持及相关依赖条件判断
    - 完善文件相关配置类的导入与注解声明

--- athena-project/athena-starter/athena-starter-file/src/main/java/com/gls/athena/starter/file/config/FileConfig.java
+++ athena-project/athena-starter/athena-starter-file/src/main/java/com/gls/athena/starter/file/config/FileConfig.java
@@ -1,6 +1,12 @@
 package com.gls.athena.starter.file.config;
 
+import com.gls.athena.starter.aliyun.oss.manager.OssFileManager;
+import com.gls.athena.starter.file.manager.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,4 +19,47 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(FileProperties.class)
 public class FileConfig {
 
+    /**
+     * 创建文件信息管理器Bean
+     * 当容器中不存在IFileInfoManager类型的Bean时，创建并注册一个基于内存的文件信息管理器
+     *
+     * @return IFileInfoManager 文件信息管理器实例
+     */
+    @Bean
+    @ConditionalOnMissingBean(IFileInfoManager.class)
+    public IFileInfoManager fileInfoManager() {
+        return new InMemoryFileInfoManager();
+    }
+
+    /**
+     * 创建本地文件存储管理器Bean
+     * 当容器中不存在IFileStorageManager类型的Bean且文件存储类型配置为local时，
+     * 创建并注册默认的文件存储管理器
+     *
+     * @param fileProperties 文件配置属性
+     * @return IFileStorageManager 文件存储管理器实例
+     */
+    @Bean
+    @ConditionalOnMissingBean(IFileStorageManager.class)
+    @ConditionalOnProperty(prefix = "athena.file", name = "type", havingValue = "local", matchIfMissing = true)
+    public IFileStorageManager fileStorageManager(FileProperties fileProperties) {
+        return new DefaultFileStorageManager(fileProperties);
+    }
+
+    /**
+     * 创建OSS文件存储管理器Bean
+     * 当容器中不存在IFileStorageManager类型的Bean且文件存储类型配置为oss，
+     * 并且OssFileManager类存在于classpath中时，创建并注册OSS文件存储管理器
+     *
+     * @param ossFileManager OSS文件管理器
+     * @return IFileStorageManager 文件存储管理器实例
+     */
+    @Bean
+    @ConditionalOnMissingBean(IFileStorageManager.class)
+    @ConditionalOnClass(OssFileManager.class)
+    @ConditionalOnProperty(prefix = "athena.file", name = "type", havingValue = "oss")
+    public IFileStorageManager fileStorageManager(OssFileManager ossFileManager) {
+        return new OssFileStorageManager(ossFileManager);
+    }
 }
+
